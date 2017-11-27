13bbdb5cdf7c6c6ed2cfa1e513a5d1cf6a8bf096
Yama <souichirou19960210@gmail.com>
2017-11-27 18:40:42 +0900

    1-11

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/top' => 'home#top'
   get '/about' => 'home#about'
 
+  root 'home#top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
