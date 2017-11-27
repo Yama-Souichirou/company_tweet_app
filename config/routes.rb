0a1f635a1a0879405a1da6d21d2b9c5c67becc11
Yama <souichirou19960210@gmail.com>
2017-11-28 03:26:57 +0900

    6-8

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/:id' => 'users#show'
   get '/signup' => 'users#new'
+  post '/users/create' => 'users#create'
 
   get '/top' => 'home#top'
   get '/about' => 'home#about'
