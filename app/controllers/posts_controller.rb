c5c65a5289a60d2476fbdbd1d5ee6f10d7c29b24
Yama <souichirou19960210@gmail.com>
2017-12-02 01:30:23 +0900

    9-4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,8 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find_by(id: params[:id])
+    @user = User.find_by(id: @post.user_id)
   end
 
   def new
