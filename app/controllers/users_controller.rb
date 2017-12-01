e133013edd4794035cc8aecfda75fd3c137753a5
Yama <souichirou19960210@gmail.com>
2017-12-01 13:02:28 +0900

    7-8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+    @user.image_name = "#{@user.id}.jpg"
+    
+    if params[:image]
+      image = params[:image]
+      File.binwrite("public/assets/img/#{@user.image_name}", image.read)
+    end
     if @user.save
       flash[:notice] = "ユーザー情報を編集しました"
       redirect_to "/users/#{@user.id}"
