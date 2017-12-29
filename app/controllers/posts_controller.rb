1721ab7299e7510bc1d1e2a2508aea0259ef54d6
Yama <souichirou19960210@gmail.com>
2017-12-29 15:07:08 +0900

    コンテンツ10章が使えなかったので 最終調整

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,38 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
-  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
-
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
+  
   def index
-    @posts = Post.all
-    .order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc)
   end
-
+  
   def show
     @post = Post.find_by(id: params[:id])
     @user = @post.user
+    @likes_count = Like.where(post_id: @post.id).count
   end
-
+  
   def new
     @post = Post.new
   end
-
+  
   def create
-    @post = Post.new(content: params[:content], user_id: @current_user.id)
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user.id
+    )
     if @post.save
       flash[:notice] = "投稿を作成しました"
-      redirect_to "/posts/index"
+      redirect_to("/posts/index")
     else
-      render 'new'
+      render("posts/new")
     end
   end
-
+  
   def edit
-    @post = Post.find(params[:id])
+    @post = Post.find_by(id: params[:id])
   end
-
+  
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
@@ -37,22 +40,23 @@ class PostsController < ApplicationController
       flash[:notice] = "投稿を編集しました"
       redirect_to("/posts/index")
     else
-      render 'edit'
+      render("posts/edit")
     end
   end
-
+  
   def destroy
-    @post = Post.find(params[:id])
+    @post = Post.find_by(id: params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました"
-    redirect_to '/posts/index'
+    redirect_to("/posts/index")
   end
-
+  
   def ensure_correct_user
     @post = Post.find_by(id: params[:id])
     if @post.user_id != @current_user.id
       flash[:notice] = "権限がありません"
-      redirect_to "/posts/index"
+      redirect_to("/posts/index")
     end
   end
+  
 end
